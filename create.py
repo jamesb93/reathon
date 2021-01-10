0f8ae3c94183402e4d4b9a68e951f0111161501f
James Bradbury <jamesbradbury93@gmail.com>
2021-01-10 19:43:27 +0000

    update basic example

--- create.py
+++ create.py
@@ -1,42 +1,15 @@
 from nodes import Session, Track, Item, Source
 
 tiger = Source(file='/Users/james/Downloads/Bengal_Tiger_aggresive_grunt_medium.wav')
+section = Source()
 session = Session()
-
+item = Item()
 session.add(
     Track(
-        Item(tiger, length=10, position=1)
+        Item(
+            tiger, length=15, position=4
+        )
     )
 )
 
-class Writer:
-    def __init__(self):
-        self.string = ''
-
-    def traverse(self, origin):
-        self.string += f'<{origin.name}\n'
-        for k, v in origin.props.items():
-                self.string += f'{k} {v}\n'
-        for node in origin.nodes:
-            self.traverse(node)
-        self.string += '>\n'
-
-    def write(self, path):
-        with open(path, "w") as f:
-            f.write(self.string)
-
-
-writer = Writer()
-
-writer.traverse(session)
-writer.write("hello.rpp")
-
-
-# session.add(
-#     Track().add(
-#         Item().add(
-#             Source()
-#         )
-#     )
-# )
-
+session.write("foobar.rpp")
