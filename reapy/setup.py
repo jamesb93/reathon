b1e1f253f2ba54be62603712be0012c3dcbe86f0
James Bradbury <jamesbradbury93@gmail.com>
2021-01-12 00:45:53 +0000

    change path to readme and desc

--- reapy/setup.py
+++ reapy/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-with open("../README.md", "r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
@@ -13,6 +13,6 @@ setup(
     author_email="jamesbradbury93@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    description="The finding things in stuff package.",
+    description="Construct REAPER projects in Python.",
     packages=find_packages()
 )
