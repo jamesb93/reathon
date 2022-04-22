255fc1e4279b611c045d378ce4e9eabf8c185f8a
Alex Harker <ajharker@gmail.com>
2022-04-22 15:03:13 +0100

    Improved marker functionality + region functionality

--- example/markers.py
+++ example/markers.py
@@ -1,5 +1,7 @@
 from reathon.nodes import *
+from reathon.helper import make_color
 from reathon.helper import marker
+from reathon.helper import region
 
 proj = Project(
     Track(isbus='1 1', trackheight='67'),
@@ -12,8 +14,8 @@ proj = Project(
 
 proj.props.extend([
     marker(1, 0, 'first'),
-    marker(2, 0.5, 'second')
-])
+    marker(2, 0.5, 'second', make_color(0, 255, 0))] # with custom color
+    + region(3, 1, 5, "region", make_color(0, 150, 150))) # add region with custom color
 
 proj.write('markers.rpp')
 
--- reathon/reathon/helper.py
+++ reathon/reathon/helper.py
@@ -1,2 +1,11 @@
-def marker(index:int, time:float, name):
-    return ['MARKER', f'{index} {time} {name}']
+
+# N.B. colors are passed as ints but should be created with this helper - pass 0 for the default colors
+
+def make_color(r, g, b):
+    return (1 << 24) + (r << 16) + (g << 8) + b
+
+def marker(index:int, time:float, name:str, color:int=0):
+    return ['MARKER', f'{index} {time} {"name"} 0 {color} 1 B']
+
+def region(index:int, start:float, end:float, name:str, color:int=0):
+    return [['MARKER', f'{index} {start} "{name}" 1 {color} 1 B'], ['MARKER', f'{index} {end} ""  1 {color} 1 B']]
