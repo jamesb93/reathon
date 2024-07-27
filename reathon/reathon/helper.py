08355f950d411383101cd611e85461d8a7a7eb6e
Leonard de Ruijter <alderuijter@gmail.com>
2024-07-27 19:46:26 +0200

    Fix marker names

--- reathon/reathon/helper.py
+++ reathon/reathon/helper.py
@@ -5,7 +5,7 @@ def make_color(r, g, b):
     return (1 << 24) + (r << 16) + (g << 8) + b
 
 def marker(index:int, time:float, name:str, color:int=0):
-    return ['MARKER', f'{index} {time} {"name"} 0 {color} 1 B']
+    return ['MARKER', f'{index} {time} "{name}" 0 {color} 1 B']
 
 def region(index:int, start:float, end:float, name:str, color:int=0):
     return [['MARKER', f'{index} {start} "{name}" 1 {color} 1 B'], ['MARKER', f'{index} {end} ""  1 {color} 1 B']]
